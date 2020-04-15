2adef1d4563d997e5b005164ae7d57072c343b53
Alex <bozeak@gmail.com>
2020-04-15 11:34:24 +0300

    LC-263 - Remove videos from preview buttons.

--- js/easyopac_collection_links.js
+++ js/easyopac_collection_links.js
@@ -32,24 +32,10 @@
                     .removeClass('previewable-pending')
                     .addClass('previewable');
 
-                  let videoId = new RegExp('[?&]v=([^&#]*)').exec(previewLink);
-                  if (videoId !== null) {
-                    $(selector + '[data-preview-id="' + previewedId + '"]', context).colorbox({
-                      iframe: true,
-                      innerWidth: "80%",
-                      innerHeight: "80%",
-                      href: function () {
-                        if (videoId && videoId[1]) {
-                          return 'https://youtube.com/embed/' + videoId[1] + '?rel=0&wmode=transparent';
-                        }
-                      }
-                    });
-                  }
                   // Add some styling for collection items.
                   $('.collections-preview--collection-wrapper').find('.previewable').parent().css('border-top', '1px solid #c6c6c6');
                 }
               });
-
             });
           }
         });
